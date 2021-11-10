e7b4a35ca1edc5e4ae0c8ac9f17e5114ed5a1cc1
anushkanema <anushkanema25@gmail.com>
2021-11-10 17:13:36 +0530

    added jsx to components

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <div>
+            <div className="donate-ban">
+                <h5 className="don-head">Support us in raising awareness</h5>
+                <div className="donbutton">
+                    <button type="button" className="don-btn">Donate</button>
+                </div>
+            </div>
+
+            <div className= "foot">
+            <p className= "f2"><span className="copyright">Foot-Prints | ©2021 All rights reserved</span><br /></p>
+            <p className= "contact">Contact Us: <a className="contact" href="mailto:anushkanema25@gmail.com">Email</a></p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer
